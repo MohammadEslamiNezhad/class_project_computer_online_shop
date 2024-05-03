10d9f97fa83990ad48f88e35dde456a1e2af4802
MohammadEslamiNezhad <164536071+MohammadEslamiNezhad@users.noreply.github.com>
2024-05-03 19:53:01 +0330

    This initial data base for bama project and I'll compleate it .

---
+++ IdeaProjects/untitled/src/DataBase.java
@@ -0,0 +1,23 @@
+public class DataBase {
+    Car [] cars = new Car[12] ;
+    void setCars(){
+        cars[0].fullNameOfCarOwner = "آرش عظیمی" ;
+        cars[0].setBrand("BMW");
+        cars[0].setModel("428i سری ۴ کروک");
+        cars[0].setBodyColor("black");
+        cars[0].setInteriorColor("black");
+        cars[0].setBodyStatus("without color");
+        cars[0].setFuelType("petrol");
+        cars[0].setKiloMeterOperation(10000);
+        cars[0].setGearBoxModel("automatic");
+        cars[0].setNumberOfGears(8);
+        cars[0].setEngineType("turbo");
+        cars[0].setNumberOfCylinder(4);
+        cars[0].setVolumeOfEngine(2f);
+        cars[0].setCarAxleModel("back axle");
+        cars[0].setEnginePower(252);
+
+        // add power and gashtavar
+    }
+
+}
